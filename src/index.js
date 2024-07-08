796dc1d55cbc5473b504b685c90872ce816acbde
kishore <kishoremalineni2023@gmail.com>
2024-07-08 15:15:12 +0530

    random-number-generate

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Counter from './Components/static';
-
+import RendomNumber from './Components';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <RendomNumber />
   </React.StrictMode>
 );
 
